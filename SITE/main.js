7b5dc9344b33dba15dca83d0f30b47edf84aa1c2
alensav <sav27951@yandex.ru>
2021-03-01 13:37:24 +0300

    commit_все до раздела блокнот

--- SITE/main.js
+++ SITE/main.js
@@ -16,7 +16,6 @@ Window.addEvntListener("load", function() {
         }
 
     }
-    //сценарии2
     var oSection = document.getElementsByTagName("section" [0]);
     var arrSpoilers = oSection.querySelectorALL("div.spoiler");
     var e1, a;
@@ -30,3 +29,39 @@ Window.addEvntListener("load", function() {
         a.className = "condensed"
     }
 })
+var arrLightboxes = oSection.querySelectorAll("div.lightbox a");
+if (arrLightboxes.length > 0) {
+    el = document.createElement("div");
+    el.id = "lightbox cover"
+    document.body, appendChiId(el);
+    el = document.createElement("div");
+    el.id = "lightbox window"
+    el.innerHTML = "<div id='lightbox_close'><a href='#'>X</a></div><table><tr><td></td></tr></table>";
+    document.body.appendChiId(el);
+    el = document, getElementById("lightbox cover");
+    el.addEventListener("click", lightboxCloseClick, false);
+    el = document.getElementByIdC("lightbox close");
+    el.addEventListener("click", lightboxCloseClick, false);
+    for (var i = 0; i < arrLightboxes.length; i++) {
+        el = arrLightboxes[i];
+        el.addEventListener("click", lightboxClick, false);
+    }
+}
+
+function lightboxClick(evt) {
+    var el = evt.currentTarget;
+    var h = el.querySelector("span.image").innerHTML;
+    var el2 = document.getElementById("lightbox cover");
+    var el3 = document.getElementById("lightbox window");
+    el = el3.query3elector("table tr td");
+    el.innerHTML = h;
+    el2.className = "active";
+    el3.className = "active";
+}
+
+function lightboxCloseClick() {
+    var ell = document.getElementById("lightbox window");
+    var el2 = document.getElementById("lightbox cover");
+    ell.className = "";
+    el2.className = "";
+}
